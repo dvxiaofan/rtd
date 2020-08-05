7d908c25008cb7c3b25f3fcb61c3c28bf2217bfd
xiaofan <1353815633@qq.com>
2020-08-05 12:52:23 +0800

    base

---
+++ basic-types.ts
@@ -0,0 +1,13 @@
+let isDone: boolean = false;
+
+let age: number = 39;
+let binaryNumber: number = 0b11111;
+
+let firstName: string = 'xiaofan';
+
+let message: string = `hello, ${firstName}, age is ${age}`;
+
+let u: undefined = undefined;
+let n: null = null;
+
+let num: number = undefined;
