4b37bb0077b5e9ebe4232749d585d4fef8f7b459
xiaofan <1353815633@qq.com>
2020-08-13 23:44:51 +0800

    泛型

--- generics.ts
+++ generics.ts
@@ -1,29 +1,55 @@
-interface IWithLength {
-	length: number;
-}
+// interface IWithLength {
+// 	length: number;
+// }
 
-function echoWithLength<T extends IWithLength>(arg: T): T {
-	console.log(arg.length);
-	return arg;
-}
+// function echoWithLength<T extends IWithLength>(arg: T): T {
+// 	console.log(arg.length);
+// 	return arg;
+// }
 
-const str = echoWithLength('str');
-const obj = echoWithLength({ length: 10, width: 90 });
-const arr2 = echoWithLength([1, 3, 5]);
+// const str = echoWithLength('str');
+// const obj = echoWithLength({ length: 10, width: 90 });
+// const arr2 = echoWithLength([1, 3, 5]);
 // echoWithLength(12);
 
-class Queue {
+class Queue<T> {
 	private data = [];
-	push(item) {
+	push(item: T) {
 		return this.data.push(item);
 	}
-	pop() {
+	pop(): T {
 		return this.data.shift();
 	}
 }
 
-const queue = new Queue();
+const queue = new Queue<number>();
 
 queue.push(1);
-queue.push('str');
-console.log(queue);
+console.log(queue.pop().toFixed());
+
+const queue2 = new Queue<string>();
+queue2.push('str');
+console.log(queue2.pop().length);
+
+interface KeyPair<T, U> {
+	key: T;
+	value: U;
+}
+
+let kp1: KeyPair<number, string> = { key: 333, value: 'tttt' };
+let kp2: KeyPair<string, number> = { key: 'hhhh', value: 444 };
+
+interface IPlus<T> {
+	(a: T, b: T): T;
+}
+
+function plus(a: number, b: number): number {
+	return a + b;
+}
+
+function connect(a: string, b: string): string {
+	return a + b;
+}
+
+const a: IPlus<number> = plus;
+const b: IPlus<string> = connect;
