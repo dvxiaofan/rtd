cffda6f468ecd137f5a542ba9ea8b63c97dec5a4
xiaofan <1353815633@qq.com>
2020-08-13 23:35:08 +0800

    0814

--- generics.ts
+++ generics.ts
@@ -10,4 +10,20 @@ function echoWithLength<T extends IWithLength>(arg: T): T {
 const str = echoWithLength('str');
 const obj = echoWithLength({ length: 10, width: 90 });
 const arr2 = echoWithLength([1, 3, 5]);
-echoWithLength(12);
+// echoWithLength(12);
+
+class Queue {
+	private data = [];
+	push(item) {
+		return this.data.push(item);
+	}
+	pop() {
+		return this.data.shift();
+	}
+}
+
+const queue = new Queue();
+
+queue.push(1);
+queue.push('str');
+console.log(queue);
